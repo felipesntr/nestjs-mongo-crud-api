1a9171bb03559fc4731f8eda851f00e2943eaac1
Felipe Santos <rfelipe273@gmail.com>
2022-01-05 03:10:13 -0300

    chore: add mongodb server for testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersController } from '@/users/users.controller';
-import { UsersService } from '@/users/user.service';
 import { UsersModule } from '@/users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AppService } from './app.service';
 @Module({
-  imports: [
-    UsersModule,
-    MongooseModule.forRoot(
-      `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@cluster0.suflu.mongodb.net/${process.env.MONGO_ATLAS_DB}?retryWrites=true&w=majority`,
-    ),
-  ],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  imports: [UsersModule, MongooseModule.forRoot(`mongodb://localhost:27017`)],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
